20390cb0ceb877b2f5b855145ec5d7191db98b54
Grant Gardner <grant@lastweekend.com.au>
2012-04-19 21:28:50 +1000

    rsync with upstream

--- spec/condition_variable_spec.rb
+++ spec/condition_variable_spec.rb
@@ -22,42 +22,5 @@ describe Strand::ConditionVariable do
     include_examples "ConditionVariable#wait"
     include_examples "ConditionVariable#broadcast"
 
-  context "calling #wait with out of order resumes" do
-    before(:each) do
-      @cond = described_class.new
-    end
-
-    it "should block until signaled" do
-      em do
-        fiber = nil
-        strand = Strand.new do
-            fiber = Fiber.current
-            @cond.wait
-            Strand.yield.should == :not_the_signal
-        end
-        fiber.resume(:not_the_signal)
-        @cond.signal
-        strand.join
-        done
-      end
-    end
-
-    it "should block until timed out" do
-      em do
-        fiber = nil
-        strand = Strand.new do
-            fiber = Fiber.current
-            @cond.wait(0.04)
-            Strand.yield.should == :not_the_signal
-        end
-        
-        EM.next_tick { fiber.resume(:not_the_signal) }
-        strand.join
-        done
-      end
-    end
-  end
-
-
 end
 
